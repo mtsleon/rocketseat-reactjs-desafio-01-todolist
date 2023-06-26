b0f4b97604e69e9628814cc25d0338a8ce06eb0f
Matheus Leonardo <mtsleon@unicamp.br>
2023-06-26 13:26:26 -0300

    configuração do projeto e parametrização de estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import styles from './App.module.css';
+
+import Logo from './assets/Logo.png';
+
+function App() {
+
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={Logo} alt="Logo todo list" />
+      </header>
+
+      <main className={styles.main}>
+
+      </main>
+    </>
+  )
+}
+
+export default App
