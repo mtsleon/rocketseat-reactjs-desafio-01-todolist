ec05ee3ec66f84c06925f46e104149eaab9116c8
Matheus Leonardo <mtsleonardo.prog@gmail.com>
2023-07-04 23:56:34 -0300

    task item implementado

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,10 +1,22 @@
 
 import styles from './TaskItem.module.css';
+import Trash from '../../assets/trash.svg';
+import { Check } from 'phosphor-react';
 
-export default function TaskItem() {
+type TaskItemProps = {
+    title: string;
+    isDone: boolean;
+}
+export default function TaskItem(props: TaskItemProps) {
     return (
         <div className={styles.taskItem}>
-            item
+            <div className={props.isDone ? styles.checkboxDone : styles.checkboxDefault}>
+                {props.isDone ? <Check size={15} className={styles.checkDoneIcon} /> : ''}
+            </div>
+            <span className={props.isDone ? styles.titleDone : styles.title}>
+                {props.title}
+            </span>
+            <div><img src={Trash} className={styles.trashIcon} /></div>
         </div>
     )
 }
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -4,14 +4,38 @@ import EmptyMessage from '../EmptyMessage/EmptyMessage';
 import TaskItem from './TaskItem';
 
 export default function TaskList() {
+    const tasks = [
+        {
+            id: 1,
+            title: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+            isDone: false,
+        },
+        {
+            id: 2,
+            title: "String urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+            isDone: true,
+        },
+        {
+            id: 3,
+            title: "Boolean urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+            isDone: true,
+        },
+    ]
     return (
         <div className={styles.taskList}>
-            
-            {/* <EmptyMessage   /> */}
-
-            <TaskItem />
-            <TaskItem />
-            <TaskItem />
+            {
+                (tasks.length !== 0) ?
+                    tasks.map(task => {
+                        return (
+                            <TaskItem
+                                key={task.id}
+                                title={task.title}
+                                isDone={task.isDone}
+                            />
+                        )
+                    })
+                    : <EmptyMessage />
+            }
         </div>
     )
 }
